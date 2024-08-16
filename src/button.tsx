f8a088cf0c498671be1dc2029ccafe2dcb9b64d1
eshellster <eshellster@gmail.com>
2024-08-16 12:19:07 +0900

    feat: enum을 const object로 사용

--- src/button.tsx
+++ src/button.tsx
@@ -1,26 +1,31 @@
 import React, { ReactNode } from "react";
 
-export enum roundedEnum {
-  sm = "2px",
-  md = "4px",
-  lg = "6px",
-  full = "2000px",
-}
+//프랍에 직관적으로 사용하기 위한 const 타입으로 설정
+export const roundedEnum = {
+  sm: "2px",
+  md: "4px",
+  lg: "6px",
+  full: "2000px",
+} as const;
 
-export enum colorSet {
-  deep = "#520922",
-  dark = "#980c28",
-  main = "#20a1d8",
-  light = "#20dbd8",
-}
+export const colorSet = {
+  deep: "#520922",
+  dark: "#980c28",
+  main: "#20a1d8",
+  light: "#20dbd8",
+} as const;
+
+// 기존에 생성한 객체를 타입으로 그대로 적용
+type RoundedEnum = typeof roundedEnum;
+type ColorSet = typeof colorSet;
 
 interface Props {
   children: ReactNode;
   paddingX?: string;
   paddingY?: string;
-  bgColor?: colorSet | string;
+  bgColor?: keyof ColorSet | string; //키값만 허용한다.
   bgOpacity?: string;
-  rounded?: roundedEnum;
+  rounded?: keyof RoundedEnum;
 }
 
 const Button: React.FC<Props> = ({
@@ -35,11 +40,11 @@ const Button: React.FC<Props> = ({
     <div
       style={{
         display: "inline-block",
-        backgroundColor: bgColor,
+        backgroundColor: bgColor && colorSet[bgColor as keyof ColorSet],
         opacity: bgOpacity,
         paddingInline: paddingX,
         paddingBlock: paddingY,
-        borderRadius: rounded,
+        borderRadius: rounded && roundedEnum[rounded],
       }}
     >
       {children}
